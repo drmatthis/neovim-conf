cb9641ec75559b435b8838d9f0877b1684b20214
Daniel Matthis <drmatthis@pm.me>
2024-10-18 10:46:47 -0500

    Reconfig. .config/nvim doesn't like symlink

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- add additional colorschemes
+  { "ellisonleao/gruvbox.nvim"},
+  { "rebelot/kanagawa.nvim"  },
+  {
+    "LazyVIM/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  }
+}
